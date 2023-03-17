48f3e49a131881c7bb88e482f80405fdf926bec5
Johnatan <natam7.costa@hotmail.com>
2023-03-17 08:47:21 -0300

    Calculator fully functional

--- script.js
+++ script.js
@@ -23,11 +23,14 @@ function operate(operator, x, y){
 };
 
 const currentDisplay = document.querySelector('#current');
+const currentOperation = document.querySelector('.currentOperation');
 const numPanel = document.querySelectorAll('.numb');
 const Delete = document.querySelector('#backspace');
-const operators = document.querySelector('.function');
+const operators = document.querySelectorAll('.function');
 const pastDisplay = document.querySelector('#past');
 const rst = document.querySelector('.result');
+const DeleteAll = document.querySelector('.delete');
+const invert = document.querySelector('.invert');
 
 let pastDisplayNumbers = '';
 let currentDisplayNumbers = '';
@@ -35,16 +38,19 @@ let operationFunction;
 
 window.addEventListener('keydown', displayNumber);
 window.addEventListener('keydown', midWay);
+window.addEventListener('keydown', (e) => e.key === 'Enter' ? result() : '');
+window.addEventListener('keydown', clear);
 numPanel.forEach( n => n.addEventListener('click', displayNumber));
 Delete.addEventListener('click', displayNumber);
 rst.addEventListener('click', result);
+operators.forEach(op => op.addEventListener('click', midWay));
+DeleteAll.addEventListener('click', clear);
+invert.addEventListener('click', invertNumber)
 
 function displayNumber(e) {
 
-    console.log(e)
-
     if(!isNaN(e.key)) {
-        if(currentDisplayNumbers.length === 17) return;
+        if(currentDisplayNumbers.length === 16) return;
         currentDisplayNumbers += e.key;
         currentDisplay.textContent = currentDisplayNumbers;
     };
@@ -57,11 +63,11 @@ function displayNumber(e) {
         currentDisplay.textContent = currentDisplayNumbers;
     };
 
-    if(!isNaN(this.textContent) || this.textContent == '.') {
-        if(currentDisplayNumbers.includes('.') && this.textContent === '.') return;
-        if(this.textContent == '.' && currentDisplayNumbers === '') currentDisplayNumbers = 0;
+    if(!isNaN(this.textContent) || this.textContent == '.' || e.key === '.') {
+        if(currentDisplayNumbers.includes('.') && (this.textContent === '.' || e.key === '.') ) return;
+        if((this.textContent == '.' || e.key === '.') && currentDisplayNumbers === '') currentDisplayNumbers = 0;
         if(currentDisplayNumbers.length === 17) return;
-        currentDisplayNumbers += this.textContent;
+        e.key === '.' ? currentDisplayNumbers += '.' : currentDisplayNumbers += this.textContent;
         currentDisplay.textContent = currentDisplayNumbers;
     };
 
@@ -77,44 +83,67 @@ function displayNumber(e) {
 
 function midWay(e) {
     
-    switch(e.key){
+    switch(e.key || e.target.textContent){
         case '+':
         operationFunction = add;
-        pastDisplayNumbers = currentDisplayNumbers;
+        if(pastDisplayNumbers === '') pastDisplayNumbers = currentDisplayNumbers;
         pastDisplay.textContent = pastDisplayNumbers;
         currentDisplayNumbers = '';
         currentDisplay.textContent = '0';
+        currentOperation.textContent = '+'
         break;
 
         case '-':
         operationFunction = subtract;
-        pastDisplayNumbers = currentDisplayNumbers;
+        if(pastDisplayNumbers === '') pastDisplayNumbers = currentDisplayNumbers;
         pastDisplay.textContent = pastDisplayNumbers;
         currentDisplayNumbers = '';
         currentDisplay.textContent = '0';
+        currentOperation.textContent = '-'
         break;
 
         case '*':
         operationFunction = multiply;
-        pastDisplayNumbers = currentDisplayNumbers;
+        if(pastDisplayNumbers === '') pastDisplayNumbers = currentDisplayNumbers;
         pastDisplay.textContent = pastDisplayNumbers;
         currentDisplayNumbers = '';
         currentDisplay.textContent = '0';
+        currentOperation.textContent = '*'
         break;
 
         case '/':
         operationFunction = divide;
-        pastDisplayNumbers = currentDisplayNumbers;
+        if(pastDisplayNumbers === '') pastDisplayNumbers = currentDisplayNumbers;
         pastDisplay.textContent = pastDisplayNumbers;
         currentDisplayNumbers = '';
         currentDisplay.textContent = '0';
+        currentOperation.textContent = '/'
         break;
     };
 };
 
 function result() {
-    pastDisplay.textContent = '';
-    currentDisplayNumbers = operate(operationFunction ,+pastDisplayNumbers, +currentDisplayNumbers);
-    currentDisplay.textContent = currentDisplayNumbers;
+    if(pastDisplayNumbers === '') return;
+    currentDisplay.textContent = '';
+    pastDisplayNumbers = operate(operationFunction ,+pastDisplayNumbers, +currentDisplayNumbers);
+    pastDisplay.textContent = pastDisplayNumbers;
+    currentDisplayNumbers = '';
 }
 
+function clear(e) {
+
+    if(!(e.key === 'Delete' || this.textContent === 'CE')) return;
+
+    pastDisplayNumbers = '';
+    currentDisplayNumbers = '';
+    delete operationFunction;
+
+    currentDisplay.textContent = '0';
+    pastDisplay.textContent = '';
+    currentOperation.textContent = '';
+};
+
+function invertNumber() {
+    currentDisplayNumbers[0] === '-' ? currentDisplayNumbers = currentDisplayNumbers.substring(1) : currentDisplayNumbers = '-' + currentDisplayNumbers;
+    currentDisplay.textContent = currentDisplayNumbers;
+};
